63a6e3b6ba2aba025a5d9ada40e713cd2a7200e3
Craig Busche <cbusche109@gmail.com>
2023-03-05 12:57:52 -0600

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,153 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+//import { Audio } from 'expo';
+import { Audio, InterruptionModeAndroid, InterruptionModeIOS } from 'expo-av';
+import { Feather } from '@expo/vector-icons';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image
+} from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 5000);
+
+
+const ukulelePic = require('./images/ukulele.png');
+const ukuleleSong =  {
+  "file": "file:./music/ukulele.mp3",
+}
+
+
+export default class App extends Component {
+  state = {
+    isPlaying: false,
+    playbackInstance: null,
+    volume: 1.0,
+    currentTrackIndex: 0,
+    isBuffering: false,
+  }
+
+	async componentDidMount() {
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      playThroughEarpieceAndroid: true,
+      //interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      interruptionModeIOS: InterruptionModeIOS.DoNotMix,
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: true,
+      //interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+      interruptionModeAndroid: InterruptionModeAndroid.DoNotMix,
+    });
+    this.loadAudio();
+  }
+
+  handlePlayPause = async () => {
+    const { isPlaying, playbackInstance } = this.state;
+    isPlaying ? await playbackInstance.pauseAsync() : await playbackInstance.playAsync();
+    this.setState({
+      isPlaying: !isPlaying
+    });
+  }
+
+
+  onPlaybackStatusUpdate = (status) => {
+    this.setState({
+      isBuffering: status.isBuffering
+    });
+  }
+
+  async loadAudio() {
+    const playbackInstance = new Audio.Sound();
+    const source = {
+      uri: ukuleleSong.file
+    }
+		const status = {
+			shouldPlay: this.state.isPlaying,
+			volume: this.state.volume,
+    };
+    playbackInstance
+      .setOnPlaybackStatusUpdate(
+        this.onPlaybackStatusUpdate
+      );
+    await playbackInstance.loadAsync(source, status, false);
+    this.setState({
+      playbackInstance
+    });
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.heading}>
+          Aloha Music
+        </Text>
+        <Text style={[styles.largeText, styles.buffer]}>
+          {this.state.isBuffering && this.state.isPlaying ? 'Buffering...' : null}
+        </Text>
+        <Image style={styles.image} source={ukulelePic}/>
+        <View style={styles.controls}>
+          <TouchableOpacity
+            style={styles.control}
+            onPress={this.handlePlayPause}
+          >
+            {this.state.isPlaying ?
+              <Feather name="pause" size={32} color="#fff"/> :
+              <Feather name="play" size={32} color="#fff"/>
+            }
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f4e3cf',
     alignItems: 'center',
     justifyContent: 'center',
+    color: '#563822',
   },
+  trackInfo: {
+    padding: 40,
+    backgroundColor: '#191A1A',
+  },
+  buffer: {
+    color: '#fff'
+  },
+  trackInfoText: {
+    textAlign: 'center',
+    flexWrap: 'wrap',
+    color: '#fff'
+  },
+  largeText: {
+    fontSize: 22
+  },
+  smallText: {
+    fontSize: 16
+  },
+  control: {
+    margin: 20,
+    color: '#563822'
+  },
+  controls: {
+    flexDirection: 'row'
+  },
+  heading: {
+    backgroundColor: '#da9547',
+    fontSize: 22,  
+    fontWeight: 'bold',
+    width: 300,
+    textAlign: 'center'
+  },
+  image: {
+    height: 500,
+    width: 300,
+
+  }
 });
